4b3de74187fc3ec240dc66e04ba0ab61fd12de0c
Accretionz <jieliu415@gmail.com>
2024-10-27 22:44:30 -0700

    url update

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,5 +1,6 @@
 const getBaseUrl = () => {
-    return "http://localhost:5000"
+    // return "http://localhost:5000"
+    return "https://book-app-backend-m70wpefm4-accretionzs-projects.vercel.app"
 }
 
 export default getBaseUrl;
